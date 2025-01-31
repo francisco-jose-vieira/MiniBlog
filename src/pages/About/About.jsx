364657d156c833d49c705867c7601140b2ed64dd
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-01-31 01:22:33 -0300

    Bloqueio de páginas que precisa de autenticação

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,21 @@
 // CSS
-import style from "./About.module.css"
+import { Link } from "react-router-dom";
+import style from "./About.module.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className={style.about}>
+      <div>
+        Sobre o MINI <span>Blog</span>
+      </div>
+      <p>
+        Este projeto consiste em um blog feito com React no front-end e Firebase
+      </p>
+      <Link to={"/posts/create"} className="btn">
+        Criar post
+      </Link>
+    </div>
+  );
+};
 
-export default About
+export default About;
